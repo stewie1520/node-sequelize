b21616143996e8e4221f915823585f774dfbf7cc
Hieu Dong <donghuuhieu1520@gmail.com>
2025-05-31 16:20:58 +0700

    fix: remove ununsed schemas
    Signed-off-by: Hieu Dong <donghuuhieu1520@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,17 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
+  globalIgnores(["dist/"]),
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    files: ["src/**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
     extends: ["js/recommended"],
   },
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    files: ["src/**/*.{js,mjs,cjs,ts,mts,cts}"],
     languageOptions: { globals: globals.node },
   },
   tseslint.configs.recommended,
--- src/index.ts
+++ src/index.ts
@@ -75,18 +75,18 @@ initStorages()
   .then(initWebsocket)
   .then(() => {
     cfonts.say("STARTED", {
-      font: "block", // define the font face
-      align: "left", // define text alignment
-      colors: ["system"], // define all colors
-      background: "transparent", // define the background color, you can also use `backgroundColor` here as key
-      letterSpacing: 1, // define letter spacing
-      lineHeight: 1, // define the line height
-      space: true, // define if the output text should have empty lines on top and on the bottom
-      maxLength: "0", // define how many character can be on one line
-      gradient: false, // define your two gradient colors
-      independentGradient: false, // define if you want to recalculate the gradient for each new line
-      transitionGradient: false, // define if this is a transition between colors directly
-      rawMode: false, // define if the line breaks should be CRLF (`\r\n`) over the default LF (`\n`)
+      font: "block",
+      align: "left",
+      colors: ["system"],
+      background: "transparent",
+      letterSpacing: 1,
+      lineHeight: 1,
+      space: true,
+      maxLength: "0",
+      gradient: false,
+      independentGradient: false,
+      transitionGradient: false,
+      rawMode: false,
       env: "node",
     });
   });
